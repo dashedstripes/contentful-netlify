478b388749809538775ed1671dd82d249aec17e5
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-07-27 09:25:42 -0500

    remove shopify references

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,14 @@
 import PostList, { Post } from './components/PostList';
 import Layout from './components/Layout';
 import { fetchPosts } from '@/services/contentful';
-import { fetchProducts } from '@/services/shopify';
-import ProductList from './components/ProductList';
 
 interface Props {
   posts: Post[];
-  products: any[]
 }
 
-const Home: React.FC<Props> = ({ posts, products }) => {
+const Home: React.FC<Props> = ({ posts }) => {
   return (
     <Layout>
-      <ProductList products={products || []}/>
       <PostList posts={posts || []} />
     </Layout>
   )
@@ -20,14 +16,10 @@ const Home: React.FC<Props> = ({ posts, products }) => {
 
 export async function getStaticProps() {
   const posts = await fetchPosts();
-  // const products = await fetchProducts();
-
-  console.log(posts);
 
   return {
     props: {
       posts,
-      products: []
     }
   }
 }
--- services/shopify.js
+++ services/shopify.js
@@ -1,43 +1,43 @@
-const URL = `${process.env.SHOPIFY_STOREFRONT_URL}/api/2023-04/graphql.json`;
+// const URL = `${process.env.SHOPIFY_STOREFRONT_URL}/api/2023-04/graphql.json`;
 
-export async function fetchProducts() {
+// export async function fetchProducts() {
 
-  const QUERY = `
-    {
-      products(first:5) {
-        edges {
-          node {
-            id
-            title
-            images(first: 1) {
-              nodes {
-                url
-                width
-                height
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
+//   const QUERY = `
+//     {
+//       products(first:5) {
+//         edges {
+//           node {
+//             id
+//             title
+//             images(first: 1) {
+//               nodes {
+//                 url
+//                 width
+//                 height
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `;
 
-  try {
-    const req = await fetch(URL, {
-      method: 'POST',
-      headers: {
-        'X-Shopify-Storefront-Access-Token': `${process.env.SHOPIFY_STOREFRONT_API_TOKEN}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ query: QUERY, variables: {} })
-    })
-    const json = await req.json();
+//   try {
+//     const req = await fetch(URL, {
+//       method: 'POST',
+//       headers: {
+//         'X-Shopify-Storefront-Access-Token': `${process.env.SHOPIFY_STOREFRONT_API_TOKEN}`,
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ query: QUERY, variables: {} })
+//     })
+//     const json = await req.json();
 
-    // console.log(json)
-    // console.log(JSON.stringify(json.data.products.edges))
+//     // console.log(json)
+//     // console.log(JSON.stringify(json.data.products.edges))
 
-    return json.data.products.edges || []
-  } catch (error) {
-    return []
-  }
-}
+//     return json.data.products.edges || []
+//   } catch (error) {
+//     return []
+//   }
+// }
